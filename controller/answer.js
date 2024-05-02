7172cd9aa64ab53dd22cde308a637fb783cf6ddc
Abelmekonn <Abelmekonn9@gmail.com>
2024-05-02 11:35:49 +0300

    "Add function to get answers by question id in answer controller and corresponding route"
    This diff adds a new function `GetAnswersByQuestionId` in the `answer` controller which fetches all answers for a given question id from

--- controller/answer.js
+++ controller/answer.js
@@ -24,6 +24,23 @@ async function createAnswer(req, res) {
     }
 }
 
+async function GetAnswersByQuestionId(req,res){
+    const questionId= req.params.questionId;
+    try {
+        const query="SELECT q.*,u.username FROM answer q INNER JOIN users u ON q.userid=u.userid WHERE questionid = ?";
+        const result =await dbConnectionPromise.query(query,[questionId])
+        if (result.length > 0) {
+            return res.status(StatusCodes.OK).json(result[0]);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).json({ msg: "Question not found" });
+        }
+    } catch (error) {
+        console.error("Error fetching question detail:", error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Something went wrong" });
+    }
+}
+
 module.exports = {
-    createAnswer
+    createAnswer,
+    GetAnswersByQuestionId
 };
--- routes/answerRoute.js
+++ routes/answerRoute.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
-const {createAnswer}  = require("../controller/answer")
+const {createAnswer,GetAnswersByQuestionId}  = require("../controller/answer")
 
 router.post("/create", authMiddleware, createAnswer);
+
+router.get('/all/:question_id',authMiddleware ,GetAnswersByQuestionId)
 
 module.exports=router;
