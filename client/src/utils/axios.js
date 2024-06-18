85a60545b16369a808b70ef1e0b4971f9d9b3833
Abelmekonn <Abelmekonn9@gmail.com>
2024-06-18 23:44:35 +0300

    axios update

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosBase=axios.create({
-    // baseURL:"https://forum-backend-dfco.onrender.com/api",
-    baseURL:"http://localhost:3000/api"
+    baseURL:"https://forum-backend-dfco.onrender.com/api",
+    // baseURL:"http://localhost:3000/api"
 })
 
 export default axiosBase;
