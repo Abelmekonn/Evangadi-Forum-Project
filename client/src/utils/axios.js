39b0710388b9834a1f86840a84349966599d0bf2
Abelmekonn <Abelmekonn9@gmail.com>
2024-06-19 22:11:12 +0300

    d

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosBase=axios.create({
-    // baseURL:"https://evangadi-forum-project-1-yb3r.onrender.com/api",
-    baseURL:"http://localhost:3306/api",
+    baseURL:"https://evangadi-forum-project-1-yb3r.onrender.com/api",
+    // baseURL:"http://localhost:3306/api",
     headers: {
         'Content-Type': 'application/json',
     },
