6f7e880acb66b57a17e345ddeac00efb7a054b77
Abelmekonn <Abelmekonn9@gmail.com>
2024-06-19 22:25:38 +0300

    n h

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import img from '../../assets/img/evangadi-logo-home.png'
-import { Link,useNavigate } from 'react-router-dom'
-import classes from './header.module.css'
+import React from 'react';
+import img from '../../assets/img/evangadi-logo-home.png';
+import { Link, useNavigate } from 'react-router-dom';
+import classes from './header.module.css';
 
 function Header() {
     const navigate = useNavigate();
-    const  handleLogout =() => {
+    const handleLogout = () => {
         localStorage.removeItem('token');
         navigate('/login');
-        window.location.reload();
-    }
+        window.location.reload(); // Consider alternative ways to refresh if needed
+    };
+
     const token = localStorage.getItem('token');
+
     return (
         <div className={classes.header_container}>
-            <div>
-                <img src={img} alt="" />
+            <div className={classes.logo}>
+                <img src={img} alt="Evangadi Logo" />
             </div>
             <div className={classes.navs}>
-                <Link to={"/"} className={classes.link}>Home</Link>
-                <Link className={classes.link}>How it works</Link>
-                    {
-                        token? (<button className={classes.btn} onClick={handleLogout}><span>Logout</span></button>) : (<button className={classes.btn}><span>Sign in</span></button>)
-                    }
+                <Link to="/" className={classes.link}>Home</Link>
+                <Link to="/" className={classes.link}>How it works</Link>
+                {token ? (
+                    <button className={classes.btn} onClick={handleLogout}><span>Logout</span></button>
+                ) : (
+                    <button className={classes.btn}><span>Sign in</span></button>
+                )}
             </div>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
